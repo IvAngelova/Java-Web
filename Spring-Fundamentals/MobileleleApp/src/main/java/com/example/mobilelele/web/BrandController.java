d287c4903358927b02972666f5c0a0f5360f0cbd
iva <iva.angelova95@gmail.com>
2022-06-10 22:02:12 +0300

    all brands page implemented

--- Spring-Fundamentals/MobileleleApp/src/main/java/com/example/mobilelele/web/BrandController.java
+++ Spring-Fundamentals/MobileleleApp/src/main/java/com/example/mobilelele/web/BrandController.java
@@ -1,24 +1,19 @@
 package com.example.mobilelele.web;
 
-import com.example.mobilelele.model.view.BrandView;
+
 import com.example.mobilelele.service.BrandService;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 public class BrandController {
 
     private final BrandService brandService;
-    private final ModelMapper modelMapper;
 
-    public BrandController(BrandService brandService, ModelMapper modelMapper) {
+    public BrandController(BrandService brandService) {
         this.brandService = brandService;
-        this.modelMapper = modelMapper;
     }
 
     @GetMapping("brands/all")
