2832e196e61261f7e2ee117589cecd4fd5656c2b
iva <iva.angelova95@gmail.com>
2022-06-14 19:36:20 +0300

    errors on invalid login

--- Spring-Fundamentals/MobileleleApp/src/main/java/com/example/mobilelele/web/UserLoginController.java
+++ Spring-Fundamentals/MobileleleApp/src/main/java/com/example/mobilelele/web/UserLoginController.java
@@ -1,7 +1,11 @@
 package com.example.mobilelele.web;
 
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class UserLoginController {
@@ -9,6 +13,21 @@ public class UserLoginController {
     @GetMapping("/users/login")
     public String login(){
         return "auth-login";
+    }
+
+    @PostMapping("/users/login-error")
+    public String failedLogin(
+            @ModelAttribute(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY)
+            String username,
+            RedirectAttributes redirectAttributes){
+
+
+        redirectAttributes.addFlashAttribute("bad_credentials", true);
+
+        redirectAttributes.addFlashAttribute("username", username);
+
+        return "redirect:/users/login";
+
     }
 
 }
